5b022d298e20308e6ec8c801bf29acd3a3bb73db
taes1g09 <thomas.ae.smith@googlemail.com>
2012-01-18 14:40:13 +0000

    Tour polished.

--- G COMP3004/Graphics CW3/behaviours.cpp
+++ G COMP3004/Graphics CW3/behaviours.cpp
@@ -75,18 +75,18 @@ void managedBehaviour::update(double delta){
 }
 
 controlBehaviour::controlBehaviour(abstractObject* parent,
-							float max_speed) : managedBehaviour(parent, max_speed) {};
+							float max_speed) : managedBehaviour(parent, max_speed) {printf("startR:\t%f\t%f\t%f\t%f\n", R[3][0], R[3][1], R[3][2], R[3][3]);
+	 printf("theta:\t%f\t\tphi:\t%f\n", theta, phi);};
 
 controlBehaviour::controlBehaviour(abstractObject* parent, glm::mat4 loc,
 							float max_speed) : managedBehaviour(parent, loc, max_speed) {/*printf("Located controlBehaviour constructed.");*/};
 
 void controlBehaviour::update(double delta) {
 	// printf("M:\t%f\t%f\t%f\t%f\n", M[3][0], M[3][1], M[3][2], M[3][3]);
-//	if (glfwGetKey( 'H' ) ) {
-		// printf("PgUp pressed.");
-//	 printf("R:\t%f\t%f\t%f\t%f\n", R[3][0], R[3][1], R[3][2], R[3][3]);
-//	 printf("theta:\t%f\t\tphi:\t%f\n", theta, phi);
-//	}
+	if (glfwGetKey( 'H' ) ) {
+	 printf("R:\t%f\t%f\t%f\t%f\n", R[3][0], R[3][1], R[3][2], R[3][3]);
+	 printf("theta:\t%f\t\tphi:\t%f\n", theta, phi);
+	}
 	managedBehaviour::update(delta);
 
 	if (belevate) {
