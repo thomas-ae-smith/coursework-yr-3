7e76640acd36adf52d4896d03cb73657397b0005
taes1g09 <taes1g09@ecs.soton.ac.uk>
2012-04-20 10:07:19 +0100

    Have refactored to refine responsibilities back to MVC system. No longer an applet, for the moment.

--- IPCG/src/Controller.java
+++ IPCG/src/Controller.java
@@ -1,26 +1,79 @@
-import java.applet.Applet;
+import java.awt.Component;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-import javax.swing.UIManager;
+public class Controller implements Runnable, KeyListener{
+	private Model model;
+	private View view;
 
-public class Controller extends Applet {
-
-	private static final long serialVersionUID = 8027372715016548764L;
-
-	public static void main(String[] args) {
-		
-		new Controller();
-	}
-	
 	public Controller() {
-		try {
-		    // Attempt to use the system's native look and feel (buttons etc)
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-	    } catch (Exception e) {
-	    	System.err.println("Error changing system look and feel.");
-	    }
-	    
-		GameWindow window = new GameWindow();
-		window.run();
-	}	
+		this.model = new Model();
+		this.view = new View();
+	}
+
+	@Override
+	public void run() {
+		// Variables for counting frames per seconds
+		long curTime = System.currentTimeMillis();
+		long lastTime = curTime;
+
+		while( true ) {
+			try {
+				// count Frames per second...
+				lastTime = curTime;
+				curTime = System.currentTimeMillis();
+
+				float delta = curTime - lastTime;
+				//	    		player.update(delta);
+				//	    		for (Active a : Active.aObjs) {
+				//	    			a.update(delta);
+				//	    		}
+				model.update(delta); //TODO: temporary
+				view.update(delta);
+
+
+				// Let the OS have a little time...
+				Thread.yield();
+			} finally {
+				view.dispose();
+			}
+		}
+	}
+
+
+	//refactor to use keys[], possibly
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+		model.setkey(e.getKeyCode(), true);
+		//		for (int i = 0; i < Constants.keycodes.MAX.ordinal(); i++) {
+		//			if(e.getKeyCode() == Constants.keycodes.values()[i].getkey() ) model.setkey(i, true);
+		//		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+		model.setkey(e.getKeyCode(), false);
+		//		for (int i = 0; i < Constants.keycodes.MAX.ordinal(); i++) {
+		//			if(e.getKeyCode() == Constants.keycodes.values()[i].getkey() ) model.setkey(i, false);
+		//		}
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public View getView() {
+		return view;
+	}
+
+	public void init() {
+		view.init(model);		
+	}
 
 }
