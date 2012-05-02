3723d5794804d74a793b4a3889453b7ac037cd9f
taes1g09 <taes1g09@ecs.soton.ac.uk>
2012-05-02 13:17:40 +0100

    About to clean up.

--- IPCG/src/Web/java.php
+++ IPCG/src/Web/java.php
@@ -1,15 +1,16 @@
 <div id="content">
 <h2 id="title-content">Java Applet</h2>
-    
-           <script src="http://www.java.com/js/deployJava.js"></script>
+<!--           <script src="http://www.java.com/js/deployJava.js"></script>
 <script>
-    var attributes = {codebase:'bin',
-                      code:'UI.GameApplet.class',
+    var attributes = {
+                      code:'GameApplet.class',
                       width:1024, height:320} ;
     var parameters = {} ;
     var version = '1.6' ;
     deployJava.runApplet(attributes, parameters, version);
 </script>
-
-Alternate embedding available <a href="fallback.html">here</a>.
+-->
+<APPLET code="GameApplet.class" archive="IPCG-web.jar" width=1024 height=320></APPLET>
+Alternate embedding available <a href="fallback.html">here</a>.<br/>
+Downloadable .jar file available on <a href="https://www.dropbox.com/s/zucnfe7ur735437/IPCG.jar">Dropbox</a>.
 </div>
